43633dda270bfcc1c2126335489e5899c434634e
melissa-24 <melissalongenberger24@gmail.com>
2022-05-16 10:48:40 -0400

    saving changes

--- js/apiData.js
+++ js/apiData.js
@@ -5,19 +5,17 @@ $(document).ready(function() {
     $('#flaskUsers').click(function(){
         $('#theFlaskUsers').animate( {
             width: 'toggle'
+        }).css({
+            display: 'flex'
         })
     })
-    // $.get(flaskUsers, function(res) {
-    //     console.log(res)
-    // })
     $('#flaskImgs').click(function(){
         $('#theFlaskImgs').animate({
             width: 'toggle'
+        }).css({
+            display: 'flex'
         })
     })
-    // $.get(flaskImgs, function(res) {
-    //     console.log(res)
-    // })
 })
 async function getFlaskUsers() {
     var response = await fetch(`${flaskUsers}`)
@@ -25,27 +23,47 @@ async function getFlaskUsers() {
     console.log("flask users: ", data.users[0])
     d = data.users
     for (var i = 0; i < d.length; i++) {
-        console.log(d)
+        // console.log(d)
+        var node = document.createElement('div')
+        var h2Name = document.createElement('h2')
+        var h2User = document.createElement('h2')
+        var namelable = document.createTextNode('Name: ')
+        var fname = document.createTextNode(d[i].firstName)
+        var userlable = document.createTextNode("Username: ")
+        var user = document.createTextNode(d[i].username)
+        h2Name.appendChild(namelable)
+        h2Name.appendChild(fname)
+        h2User.appendChild(userlable)
+        h2User.appendChild(user)
+        node.appendChild(h2Name)
+        node.appendChild(h2User)
+        document.getElementById('theFlaskUsers').appendChild(node)
     }
 }
 
 async function getFlaskImgs() {
     var response = await fetch(`${flaskImgs}`)
     var data = await response.json()
-    // console.log("flask imgs: ", data.imgs)
+    console.log("flask imgs: ", data.imgs)
     d = data.imgs
     for (var i = 0; i < d.length; i++) {
         // console.log(d)
         var node = document.createElement('div')
         var img = new Image()
         var h2 = document.createElement('h2')
+        var h3 = document.createElement('h3')
         var name = document.createTextNode(d[i].name)
+        var username = document.createTextNode(d[i].user)
+        var userLabel = document.createTextNode("Added by: ")
         // console.log(name)
-        img.src = `${d[i].img}`
-        img.alt = name
+        img.src = `${d[i].imgUrl}`
+        img.alt = `${d[i].name}`
         h2.appendChild(name)
+        h3.appendChild(userLabel)
+        h3.appendChild(username)
         node.appendChild(img)
         node.appendChild(h2)
+        node.appendChild(h3)
         document.getElementById('theFlaskImgs').appendChild(node)
     }
 }
--- js/scripts.js
+++ js/scripts.js
@@ -33,46 +33,25 @@ $(document).ready(function(){
     })
 })
 
-async function getToons() {
+async function getTunes() {
      var response = await fetch(`${toons}`)
      var data = await response.json()
-     console.log("full toon api data:", data)
+     console.log("full tune api data:", data)
      for (var i = 0; i < data.length; i++) {
         // console.log(data[i])
         var node = document.createElement('div')
         var h2 = document.createElement('h2')
         var h3 = document.createElement('h3')
-        // var form = document.createElement('form')
-        // var nameInput = document.createElement('input')
-        // var imgInput = document.createElement('input')
-        // var button = document.createElement('button')
-        // var submit = document.createTextNode('Save Image to Database')
-        // button.appendChild(submit)
         var img = new Image()
         img.src = `${data[i].img}`
         img.alt = `${data[i].name}`
         var name = document.createTextNode(data[i].name)
         h2.appendChild(name)
         var birth = document.createTextNode(data[i].birthDay)
-        // form.setAttribute('method', 'post')
-        // form.setAttribute('action', '/images/create/')
-        // nameInput.setAttribute('type', 'hidden')
-        // imgInput.setAttribute('type', 'hidden')
-        // namesrc = `${data[i].name}`
-        // imgsrc = `${data[i].img}`
-        // nameInput.setAttribute('name', 'name')
-        // imgInput.setAttribute('name', 'img')
-        // nameInput.setAttribute('value', namesrc)
-        // imgInput.setAttribute('value', imgsrc)
-        // form.appendChild(nameInput)
-        // form.appendChild(imgInput)
-        // form.appendChild(button)
-        // console.log("name and img only: ", namesrc, imgsrc)
         h3.appendChild(birth)
         node.appendChild(h2)
         node.appendChild(h3)
         node.appendChild(img)
-        // node.appendChild(form)
         document.getElementById('tune').appendChild(node)
     }
 }
