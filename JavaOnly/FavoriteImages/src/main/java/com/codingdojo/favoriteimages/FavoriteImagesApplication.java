395d806e6e796e4184a85b065c63fd0bddc5195f
Melissa Longenberger <apple@Irish-iMac.hitronhub.home>
2022-05-24 20:25:17 -0400

    saving java start

---
+++ JavaOnly/FavoriteImages/src/main/java/com/codingdojo/favoriteimages/FavoriteImagesApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.favoriteimages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteImagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteImagesApplication.class, args);
+	}
+
+}
---
+++ JavaOnly/FavoriteImages/src/test/java/com/codingdojo/favoriteimages/FavoriteImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.favoriteimages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteImagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
