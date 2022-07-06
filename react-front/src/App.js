c40db45906f24686cd23f493d401df546c988618
melissa-24 <melissalongenberger24@gmail.com>
2022-07-06 19:55:06 -0400

    saivng

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react'
-
+import { Route, Link, Routes } from "react-router-dom";
 import './css/styles.css'
 import Header from './components/base/header'
 import Footer from './components/base/footer'
+import Main from './components/main'
 
-const App = () => {
+export default function App() {
   return (
     <div className="App">
       <header>
         <Header />
       </header>
       <main>
+      <Routes>
 
+        <Route path="/" component={Main} />
+      </Routes>
       </main>
       <footer>
         <Footer />
@@ -19,5 +23,3 @@ const App = () => {
     </div>
   );
 };
-
-export default App;
---
+++ react-front/src/components/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <>
+        <h1>Favorite Images</h1>
+        <p>Please chose which backend you wish to use.  You can create a user on all of them or just one.  Please feel free to checkout the other applications that are also a part of this project</p>
+        </>
+    )
+}
+
+export default Main
