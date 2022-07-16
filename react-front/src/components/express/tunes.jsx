5f55554c927e039560065a55c3d7a07dac23ecbc
melissa-24 <melissalongenberger24@gmail.com>
2022-07-16 15:39:07 -0400

    now showing tunes on the express side of react-front

--- react-front/src/components/express/tunes.jsx
+++ react-front/src/components/express/tunes.jsx
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import LoggedHeader from './base/loggedHeader'
 
+const tunes = 'https://dojo.navyladyveteran.com/characters/'
+console.log(tunes)
 const Tunes = () => {
+    const [tuneData, setTuneData] = useState([])
+    useEffect(() => {
+        axios
+            .get(`https://dojo.navyladyveteran.com/characters/`)
+            // .then((res) => console.log( res.data))
+            .then((res) => setTuneData(res.data))
+            .catch((err) => console.error(err))
+    }, [])
+
+    function getTunes() {
+        console.log("button clicked")
+        var el = document.getElementById('tune')
+        // el.style.display = 'flex'
+        if (el.style.display === 'flex') {
+            el.style.display = 'none'
+        } else {
+            el.style.display = 'flex'
+        }
+        }
+
     return (
         <>
         <header>
             <LoggedHeader />
         </header>
+        <main>
+            <button id='button' onClick={getTunes}>Get The Looney Tunes</button>
+            <div id="tune" className="api">
+                {console.log(tuneData)}
+                {tuneData.map((tune) => (
+                    <div>
+                        <h2>{tune.name}</h2>
+                        <h3>{tune.birthDay}</h3>
+                        <img src={tune.img} alt={tune.name} />
+                        <form action="">
+                            <button>Save Image to Database</button>
+                        </form>
+                    </div>
+                ))}
+            </div>
+        </main>
         </>
     )
 }
